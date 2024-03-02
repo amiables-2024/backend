9338e95742687fc10f9724b55f83b3fe9755e856
jl33-ai <justinkhlee27@gmail.com>
2024-03-02 22:15:56 +1100

    chat functionality

--- src/server.ts
+++ src/server.ts
@@ -36,6 +36,25 @@ app.use(express.static(path.join(__dirname, '../src/public')));
 app.use(routesRouter);
 
 io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+    socket.on("join_room", (roomId) => {
+      socket.join(roomId);
+      console.log(`user with id-${socket.id} joined room - ${roomId}`);
+    });
+  
+    socket.on("send_msg", (data) => {
+      console.log(data, "DATA");
+      //This will send a message to a specific room ID
+      socket.to(data.roomId).emit("receive_msg", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
+
+
+    // justin's code above
+
     socket.emit("me", socket.id);
 
     socket.on("disconnect", () => {
