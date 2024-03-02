c972f7599dc1d9817d43e9846ba035836d6bcb4a
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-03-02 12:43:28 +1100

    Started working on Socket backend for Video/Audio calls

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,23 @@ import 'dotenv/config'
 import "reflect-metadata"
 
 import express, {Express} from 'express';
+import * as http from "http";
+import * as socket from "socket.io";
 import cors from "cors";
 import {routesRouter} from "./routes/routes";
 import {initDatabase} from "./database/database";
 
 const app: Express = express();
+const server = http.createServer(app);
+const io = new socket.Server(server, {
+    cors: {
+        origin: "https://main.d3i9env3ux4lc9.amplifyapp.com",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(cors({
-    origin: "*",
+    origin: "https://main.d3i9env3ux4lc9.amplifyapp.com",
     credentials: true,
     optionsSuccessStatus: 200
 }))
@@ -18,6 +27,26 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(routesRouter);
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", {
+            signal: data.signalData,
+            from: data.from,
+            name: data.name
+        })
+    })
+
+    socket.on("answerCall", data => io.to(data.to).emit("callAccepted", {
+        signal: data.signal
+    }))
+});
 
 initDatabase()
     .then(() => {
